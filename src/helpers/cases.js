fbeeb962812503dff6ef1e0653a097e45fef39f6
paulparamono <sereuspro94@gmail.com>
2021-12-13 17:02:44 +0600

    adduser

---
+++ src/helpers/cases.js
@@ -0,0 +1,8 @@
+export const CASE_GET_PRODUCTS = "GET_PRODUCTS";
+export const CASE_GET_ONE_PRODUCT = "GET_ONE_PRODUCT";
+
+export const CASE_GET_BRANDS = 'GET_BRANDS';
+
+export const CASE_GET_CART = "GET_CART";
+
+export const CASE_GET_FAVORITES = "GET_FAVORITES";
