cb6cf721ca463d4e1131a365e3e6bbc7638f2c10
emir <emir6816@gmail.com>
2021-12-14 13:21:46 +0600

    home part№1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
+import AuthContextProvider from "./contexts/authContext";
+import ProductsContextProvider from "./contexts/productsContext";
+import BrandsContextProvider from "./contexts/brandsContext";
+import CartContextProvider from "./contexts/cartContext";
+import FavoritesContextProvider from "./contexts/FavoritesContext";
+
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+
+import Routing from "./Routing";
+
+import "antd/dist/antd.css";
+import "./App.css";
+
+const App = () => {
   return (
-  <div></div>
+    <AuthContextProvider>
+      <CartContextProvider>
+      <FavoritesContextProvider>
+        <BrandsContextProvider>
+          <ProductsContextProvider>
+            <BrowserRouter>
+              <Header />
+              <Routing />
+              <Footer />
+            </BrowserRouter>
+          </ProductsContextProvider>
+        </BrandsContextProvider>
+      </FavoritesContextProvider>
+      </CartContextProvider>
+    </AuthContextProvider>
+    
   );
-}
+};
 
 export default App;
+
+
+/*
+CRUD
+фильтрация
+пагинация
+поиск
+корзина
+избранное
+роутинг
+админка
+*/
